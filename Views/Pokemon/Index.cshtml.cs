9e115d8aba3575d3866cd2ac5abddf5848a3a2a4
Mirabueno-Lanz-Manuel-BSCS32E1 <162534195+Mirabueno-Lanz-Manuel-BSCS32E1@users.noreply.github.com>
2024-06-01 05:41:03 +0800

    Add files via upload

---
+++ Views/Pokemon/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokemonApp.Views.Pokemon
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
